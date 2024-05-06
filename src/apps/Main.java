3a27518141e072bcd92671ff581f0500a90f95e3
Gregory Linville <gll4kc@umsystem.edu>
2024-05-05 21:53:45 -0500

    Algorithm

--- src/apps/Main.java
+++ src/apps/Main.java
@@ -9,34 +9,37 @@ import org.apache.commons.csv.CSVPrinter;
 public class Main {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-		long startTime, endTime;
-		int[] largeSortedArray = LargeArrayGenerator.generateSortedArray(1000000);
-		int sortedTarget = largeSortedArray[random.nextInt(1000000)];
+		for (int i = 0; i < 11; i++) {
+			Random random = new Random();
+			long startTime, endTime;
 
-		try {
-			FileWriter fileWriter = new FileWriter("search_performance.csv", true);
-			@SuppressWarnings("deprecation")
-			CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT);
+			try {
 
-			// Perform Linear Search
-			startTime = System.nanoTime();
-			int linearSearchIndex = LinearSearch.linearSearch(largeSortedArray, sortedTarget);
-			endTime = System.nanoTime();
-			csvPrinter.printRecord("Linear Search", sortedTarget, linearSearchIndex, endTime - startTime);
+				FileWriter fileWriter = new FileWriter("search_performance.csv", true);
+				@SuppressWarnings("deprecation")
+				CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT);
 
-			// Perform Jump Search
-			startTime = System.nanoTime();
-			int jumpSearchIndex = JumpSearch.jumpSearch(largeSortedArray, sortedTarget);
-			endTime = System.nanoTime();
-			csvPrinter.printRecord("Jump Search", sortedTarget, jumpSearchIndex, endTime - startTime);
+				int[] largeSortedArray = LargeArrayGenerator.generateSortedArray(1000000);
+				int sortedTarget = largeSortedArray[random.nextInt(1000000)];
+				// Perform Linear Search
+				startTime = System.nanoTime();
+				int linearSearchIndex = LinearSearch.linearSearch(largeSortedArray, sortedTarget);
+				endTime = System.nanoTime();
+				csvPrinter.printRecord("Linear Search", sortedTarget, linearSearchIndex, endTime - startTime);
 
-			csvPrinter.flush(); // Ensure all data is written to the file
-			csvPrinter.close(); // Close the printer to release resources
-			fileWriter.close(); // Close the writer to release resources
-			System.out.println("Data has been written to CSV.");
-		} catch (IOException e) {
-			e.printStackTrace();
+				// Perform Jump Search
+				startTime = System.nanoTime();
+				int jumpSearchIndex = JumpSearch.jumpSearch(largeSortedArray, sortedTarget);
+				endTime = System.nanoTime();
+				csvPrinter.printRecord("Jump Search", sortedTarget, jumpSearchIndex, endTime - startTime);
+
+				csvPrinter.flush(); // Ensure all data is written to the file
+				csvPrinter.close(); // Close the printer to release resources
+				fileWriter.close(); // Close the writer to release resources
+				System.out.println("Data has been written to CSV.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
